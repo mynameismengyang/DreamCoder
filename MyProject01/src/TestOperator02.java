016af26af477e11c1b659a6aef50fd390b899a56
MengYang-Lucky <1018010504@njupt.edu.cn>
2019-09-27 20:05:25 +0800

    Update

--- MyProject01/src/TestOperator02.java
+++ MyProject01/src/TestOperator02.java
@@ -37,6 +37,38 @@ public class TestOperator02 {
 		System.out.println(bb+nn+cc);
 		System.out.println(nn+cc+bb);
 		
+		/**
+		 * 移位(shift)
+		 * During the interview, the interviewer will ask, 3*2 is the fastest way to calculate.
+		 */
+		int mul=3<<1;
+		System.out.println(mul);
+		int division= 21>>1;
+		System.out.println(division);
+		
+		/**
+		 * 关系操作符(relational operator)
+		 * ==、!= 比较的是对象的引用，对象的引用就是别名
+		 */
+		
+		Integer n1 = new Integer(47);
+		Integer n2 = new Integer(47);
+		System.out.println(n1 == n2);//false
+		System.out.println(n1 != n2);//true
+		System.out.println(n1.equals(n2));//true
+		
+		/**
+		 * revise
+		 */
+		int i = 1;
+		System.out.println("i:" + i);//1
+		System.out.println("++i:" + ++i);//2
+		System.out.println("i++:" + i++);//2
+		System.out.println("i:" + i);//3
+		System.out.println("--i:" + --i);//2
+		System.out.println("i--:" + i--);//2
+		System.out.println("i:" + i);//1
+		
 	}
 
 }
