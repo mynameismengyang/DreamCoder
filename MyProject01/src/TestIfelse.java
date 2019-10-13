d9bc97ded16826c0efd9dbf5b20c8044b945f634
MengYang-Lucky <1018010504@njupt.edu.cn>
2019-10-13 22:23:13 +0800

    TestIfElse

--- MyProject01/src/TestIfelse.java
+++ MyProject01/src/TestIfelse.java
@@ -1,4 +1,17 @@
 
 public class TestIfelse {
-
+	public static void main(String[] args) {
+		
+		double r = 4 * Math.random();
+		double area = Math.PI*Math.pow(r,2);
+		double circle = 2*Math.PI*r;
+		System.out.println(area);
+		System.out.println(circle);
+		if(area > circle) {
+			System.out.println("面积大于周长");
+		}
+		if(area < circle) {
+			System.out.println("面积小于周长");
+		}
+	}
 }
