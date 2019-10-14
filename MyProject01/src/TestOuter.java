30f82117023d5abf369ea4526ce87265f79b5509
MengYang-Lucky <1018010504@njupt.edu.cn>
2019-10-14 22:17:18 +0800

    Update TestOuter.java

--- MyProject01/src/TestOuter.java
+++ MyProject01/src/TestOuter.java
@@ -1,14 +1,14 @@
 
 public class TestOuter {
-	public static void mian(String[] args) {
+	public static void main(String[] args) {
 		//打印101-150之间所有的质数
 		outer:for (int i = 101; i < 150; i++) {
-			for(int j = 2; j<i/2;j++) {
+			for(int j = 2; j<=i/2;j++) {//一个数一般
 				if(i%j == 0) {
 					continue outer;
 				}
 			}
-			System.out.print(i + "");
+			System.out.print(i + " ");
 		}
 	}
 }
